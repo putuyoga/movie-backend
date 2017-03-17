96627cd92c072a2a8853bd6126431e1c87619ea4
Bao Tran Duc <bao.tran@pyramid-consulting.com>
2017-03-17 14:03:05 +0700

    finished unit test for controller

--- test/specs/controllers/movieController.spec.js
+++ test/specs/controllers/movieController.spec.js
@@ -2,6 +2,8 @@ const sinon = require('sinon');
 const chai = require('chai');
 const sinonChai = require('sinon-chai');
 var chaiAsPromised = require("chai-as-promised");
+const Boom = require('Boom');
+
 const movieController = require('../../../src/controllers/movieController');
 const movieService = require('../../../src/services/movieService');
 
@@ -22,6 +24,71 @@ describe('Get Movies API', () => {
             movieService.getMovies.restore();
             done();
 		}).catch(error => {
+            movieService.getMovies.restore();
+            done(error);
+        });
+	});
+
+    it("should return movie array", (done) => {
+        const movieArray = [
+            {    
+                name: 'Kong Skull Island',
+                year: 1980 ,
+                createdDate: Date(),
+                movieDetail: {
+                    directorName: 'Jordan Vogt-Roberts',
+                    language: 'English',
+                    country: 'US',
+                    genre: ['action', 'romantic'],
+                    rating: 5,
+                    vote: 100000
+                }
+            },
+            {    
+                name: 'Kong Skull Island Part 2',
+                year: 1981 ,
+                createdDate: Date(),
+                movieDetail: {
+                    directorName: 'Jordan Vogt-Roberts',
+                    language: 'English',
+                    country: 'US',
+                    genre: ['action', 'romantic'],
+                    rating: 4,
+                    vote: 1000
+                }
+            }
+        ];
+		sinon.stub(movieService, 'getMovies', () => Promise.resolve(movieArray));
+		const reply = sinon.spy();
+		movieController.getMovies({
+            log: () => null,
+            query: {}
+        }, reply)
+		.then(() => {
+			reply.should.have.been.calledWith(movieArray);
+            movieService.getMovies.restore();
+            done();
+		}).catch(error => {
+            movieService.getMovies.restore();
+            done(error);
+        });
+	});
+
+    it("should return error if could not get movie list", (done) => {
+		sinon.stub(movieService, 'getMovies', () => Promise.reject({
+            message: 'Could not get movie list',
+            code: 'failedToGetMovieList'
+        }));
+		const reply = sinon.spy();
+		movieController.getMovies({
+            log: () => null,
+            query: {}
+        }, reply)
+		.then(() => {
+            movieService.getMovies.restore();
+            done();
+		}).catch(error => {
+            reply.should.have.been.calledWith(Boom.badImplementation('Failed to get movies', error));
             movieService.getMovies.restore();
             done(error);
         });
