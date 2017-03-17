6f6d5e188bfac399f63f257e5cea5e93f09277f6
Bao Tran Duc <bao.tran@pyramid-consulting.com>
2017-03-17 10:26:13 +0700

    add logging

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const Hapi = require('hapi');
 const Inert = require('inert');
 const Vision = require('vision');
 const HapiSwagger = require('hapi-swagger');
+const Good = require('Good');
 
 const Pack = require('./package');
 const configureRoutes = require('./src/routes');
 
-const hostname = 'localhost';
+const hostname = '0.0.0.0';
 const port = 3000;
 const db = require('./config/db');
 
@@ -16,19 +17,35 @@ server.connection({
     port: port
 });
 
-const options = {
+const swaggerOptions = {
     info: {
         'title': 'Movie API Documentation',
         'version': Pack.version,
     }
 };
 
+const reporterOptions = {
+    reporters: {
+        myConsoleReporter: [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{ error: '*', response: '*', request: '*' }]
+        }, {
+            module: 'good-console'
+        }, 'stdout']
+    }
+}
+
 server.register([
     Inert,
     Vision,
     {
         'register': HapiSwagger,
-        'options': options
+        'options': swaggerOptions
+    },
+    {
+        'register': Good,
+        'options': reporterOptions
     }], (err) => {
         if(err){
             server.error(`Something went wrong, can't start server.`);
--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -3,19 +3,35 @@ const movieService = require('../services/movieService');
 const Boom = require('Boom');
 
 const getMovies = (request, reply) => {
+    // Log request
+    request.log(['Incomming request']);
+
     const page = request.query.page ? request.query.page : 1;
     
     movieService.getMovies(page, movieRepositoryObj.getMovies)
-    .then(movies => reply(movies))
-    .catch(error => reply(Boom.badImplementation('Failed to get movies', error)));
+    .then(movies => {
+        console.log('Reponse payload:', movies);
+        return reply(movies);
+    })
+    .catch(error => {
+        console.log(error);
+        return reply(Boom.badImplementation('Failed to get movies', error));
+    });
 }
 
 const getMovieDtl = (request, reply) => {
+    // Log request
+    request.log(['Incomming request']);
+
     const movieId = request.params.id;
 
     movieService.getMovie(movieId, movieRepositoryObj.getMovie)
-    .then(movie => reply(movie))
+    .then(movie => {
+        console.log('Reponse payload:', movie);
+        return reply(movie);
+    })
     .catch(error => {
+        console.log(error);
         if(error.code === 'NoSuchMovie'){
             return reply(Boom.notFound('Movie not found', error));
         } else {
